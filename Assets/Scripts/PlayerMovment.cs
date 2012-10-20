058607c52f334185496d94f1ad705735132f7a08
Andrey Korotkov aka DRON <dron@dronprogs.org>
2012-10-20 16:57:22 +0400

    Running tank.

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -2,32 +2,75 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerMovment : MonoBehaviour {
+	
+	private const float TANK_SPEED = 1.0f;
+	private const float SCENE_SIZE = 240.0f;
+	
+	private Vector3 oldPos;
+	
+ 	void OnTriggerEnter(Collider other) {
+		gameObject.transform.position = oldPos;
+    }
 
-	// Use this for initialization
+	void OnTriggerStay(Collider other) {
+		gameObject.transform.position = oldPos;
+    }
+	
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate () {
 		
 		Vector3 pos = gameObject.transform.position;
 		
+		oldPos = pos;
+
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			gameObject.transform.Translate(pos + new Vector3(10.0, 0.0, 0.0));
+			pos.x -= TANK_SPEED;
+			
+			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+
+			if (pos.x < -SCENE_SIZE)
+				pos.x = -SCENE_SIZE;
 		}
-		
+		else
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-		}
+			pos.x += TANK_SPEED;
+			
+			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+			gameObject.transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
 
+			
+			if (pos.x > SCENE_SIZE)
+				pos.x = SCENE_SIZE;
+		}
+		else
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
-		}
+			pos.z += TANK_SPEED;
 
+			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+			gameObject.transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
+			
+			if (pos.z > SCENE_SIZE)
+				pos.z = SCENE_SIZE;
+		}
+		else
 		if (Input.GetKey(KeyCode.DownArrow))
 		{
+			pos.z -= TANK_SPEED;
+			
+			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
+			gameObject.transform.Rotate(new Vector3(0.0f, -90.0f, 0.0f));
+
+			
+			if (pos.z < -SCENE_SIZE)
+				pos.z = -SCENE_SIZE;
 		}
-}
+
+		gameObject.transform.position = pos;
+	}
 }
