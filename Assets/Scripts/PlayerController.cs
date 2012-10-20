8e88aa92a38f8aeff9d0d0a9cbc0414f6ea9f372
Yakov Negrey <jashik.ru@gmail.com>
2012-10-20 23:20:30 +0400

    fix PlayerController.cs

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+	private Engine engine;
+	private bool inited = false;
+	// Use this for initialization
+	void Start()
+	{
+		engine = gameObject.GetComponent<Engine>();
+		if (engine != null) inited = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (!inited)
+			return;
+
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			engine.setMovement(MovmentType.Left);
+		}
+		else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			engine.setMovement(MovmentType.Right);
+		}
+		else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+		{
+			engine.setMovement(MovmentType.Down);
+		}
+		else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			engine.setMovement(MovmentType.Up);
+		}
+		else
+		{
+			engine.setMovement(MovmentType.Stop);
+		}
+
+		if (Input.GetKey(KeyCode.Space)) {
+			engine.shoot();
+		}
+	}
+}
