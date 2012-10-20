37c4a29aaef3d18a6a7974738b2e7cce1d4baa55
Andrey Korotkov aka DRON <dron@dronprogs.org>
2012-10-21 00:15:05 +0400

    Merge2.

--- Assets/Scripts/BulletFly.cs
+++ Assets/Scripts/BulletFly.cs
@@ -5,22 +5,33 @@ public class BulletFly : MonoBehaviour
 {
 
 	public int team;
+	public GameObject currentDetonator;
+	public GameObject owner;
 	private const float BULLET_SPEED = 2.5f;
 
 	void Start()
 	{
 
 	}
-
-	void OnTriggerEnter(Collider other)
-	{
-		DestroyObject(gameObject);
-	}
+	
+	 void OnTriggerEnter(Collider other) {
+		
+		//if (other.gameObject.GetComponent<Block>() != null)
+		
+		//Instantiate (Detonator,other.gameObject.transform, Quaternion.identity);
+		
+		//(gameObject.GetComponent<Detonator>() as Detonator).Explode();
+			DestroyObject(other.gameObject);
+		
+			DestroyObject(this.gameObject);
+		//(owner.GetComponent<PlayerMovment>() as PlayerMovment).bullsCnt--;
+    }
 
 	void FixedUpdate()
 	{
 
 		float yaw = transform.rotation.eulerAngles.y;
+
 		Vector3 pos = transform.position;
 		pos.x -= Mathf.Cos(Mathf.Deg2Rad * yaw) * BULLET_SPEED;
 		pos.z += Mathf.Sin(Mathf.Deg2Rad * yaw) * BULLET_SPEED;
