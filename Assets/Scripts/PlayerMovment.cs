322bc914e433a23c5c5e102902332b72fe032ae2
Andrey Korotkov aka DRON <dron@dronprogs.org>
2012-10-20 18:16:15 +0400

    Rolling shooting.

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -3,10 +3,14 @@ using System.Collections;
 
 public class PlayerMovment : MonoBehaviour {
 	
+	public GameObject bullet;
+	public int bullsCnt;
+	
 	private const float TANK_SPEED = 1.0f;
-	private const float SCENE_SIZE = 240.0f;
+	public const float SCENE_SIZE = 240.0f;
 	
 	private Vector3 oldPos;
+	private float angleY;
 	
  	void OnTriggerEnter(Collider other) {
 		gameObject.transform.position = oldPos;
@@ -17,7 +21,7 @@ public class PlayerMovment : MonoBehaviour {
     }
 	
 	void Start () {
-	
+		bullsCnt = 0;
 	}
 
 	void FixedUpdate () {
@@ -30,6 +34,7 @@ public class PlayerMovment : MonoBehaviour {
 		{
 			pos.x -= TANK_SPEED;
 			
+			angleY = 0.0f;
 			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
 
 			if (pos.x < -SCENE_SIZE)
@@ -40,8 +45,9 @@ public class PlayerMovment : MonoBehaviour {
 		{
 			pos.x += TANK_SPEED;
 			
+			angleY = 180.0f;
 			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
-			gameObject.transform.Rotate(new Vector3(0.0f, 180.0f, 0.0f));
+			gameObject.transform.Rotate(new Vector3(0.0f, angleY, 0.0f));
 
 			
 			if (pos.x > SCENE_SIZE)
@@ -51,9 +57,10 @@ public class PlayerMovment : MonoBehaviour {
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			pos.z += TANK_SPEED;
-
+			
+			angleY = 90.0f;
 			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
-			gameObject.transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
+			gameObject.transform.Rotate(new Vector3(0.0f, angleY, 0.0f));
 			
 			if (pos.z > SCENE_SIZE)
 				pos.z = SCENE_SIZE;
@@ -63,14 +70,23 @@ public class PlayerMovment : MonoBehaviour {
 		{
 			pos.z -= TANK_SPEED;
 			
+			angleY = -90.0f;
 			gameObject.transform.rotation = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);
-			gameObject.transform.Rotate(new Vector3(0.0f, -90.0f, 0.0f));
+			gameObject.transform.Rotate(new Vector3(0.0f, angleY, 0.0f));
 
 			
 			if (pos.z < -SCENE_SIZE)
 				pos.z = -SCENE_SIZE;
 		}
 
+		if (Input.GetKey(KeyCode.Space) && bullsCnt == 0)
+		{
+			GameObject new_bullet = Instantiate(bullet, oldPos, Quaternion.Euler(new Vector3(0, angleY, 0))) as GameObject;
+			BulletFly bf = new_bullet.GetComponent<BulletFly>() as BulletFly;
+			bf.owner = this.gameObject;
+			bullsCnt++;
+		}
+			
 		gameObject.transform.position = pos;
 	}
 }
