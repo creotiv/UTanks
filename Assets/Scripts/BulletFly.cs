f4c148d30f0fc7a52ee1b30771c3be0ad88d64f3
Andrey Korotkov aka DRON <dron@dronprogs.org>
2012-10-20 17:10:24 +0400

    Some forgotten stuff.

---
+++ Assets/Scripts/BulletFly.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class BulletFly : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	
+	}
+}
